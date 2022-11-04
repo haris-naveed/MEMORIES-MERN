bab9bf311c18c5fffcc419544e40151e34cb7095
haris-naveed <harisnaveed1997@gmail.com>
2022-11-04 16:49:32 +0500

    authentication fron end

---
+++ client/src/components/Auth/Input.js
@@ -0,0 +1,34 @@
+import { Grid, IconButton, InputAdornment, TextField } from '@material-ui/core'
+import React from 'react'
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
+const Input = ({name,half,handleChange,label,autoFocus,type,handleShowPassword}) => {
+  return (
+    <Grid item xs={12} sm={half?6:12}>
+        <TextField
+        name={name}
+        onChange={handleChange}
+        variant='outlined'
+        required
+        fullWidth
+        label={label}
+        autoFocus={autoFocus}
+        type={type}
+        InputProps={name==='password' ? {
+          endAdornment:(
+                <InputAdornment position='end'>
+                   <IconButton onClick={handleShowPassword}>
+                        {type==='password'?<VisibilityIcon/>:<VisibilityOffIcon/>}
+                   </IconButton>
+                </InputAdornment>
+            )
+        }:null
+
+        }
+        />
+
+    </Grid>
+  )
+}
+
+export default Input
